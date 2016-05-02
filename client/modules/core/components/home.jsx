70fca55254975ff04da5d43b44ccec4a1dcd587a
excalliburbd <excalliburbd@gmail.com>
2016-05-02 08:14:56 +0600

    added react-bootstrap and favicons

---
+++ client/modules/core/components/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Jumbotron, Button } from 'react-bootstrap';
+
+const Home = () => (
+  <div>
+    <Jumbotron>
+      <h1>Shaswoto Bidyangon</h1>
+      <p>Let's be self educated</p>
+      <p><Button bsStyle="primary">Learn more</Button></p>
+    </Jumbotron>
+  </div>
+);
+
+export default Home;
